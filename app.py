fe91eb19f9e99b140cf43ba344da2cce7e0e0f78
unknown <uehararyuta0209@gmail.com>
2026-08-27 17:04:25 +0900

    Day4: parameter control from UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,send_file
+from flask import Flask,render_template,send_file,request
 import matplotlib
 matplotlib.use('Agg') 
 import matplotlib.pyplot as plt
@@ -13,8 +13,10 @@ def index():
 
 @app.route('/graph')
 def graph():
+    length = float(request.args.get('length', 1.0))
+    angle  = float(request.args.get('angle', 30.0))
 # ここにグラフを生成して返す処理
-    t, theta, omega = simulator(length=1.0, angle_deg=30.0, duration=10.0)
+    t, theta, omega = simulator(length=length, angle_deg=angle, duration=10.0)
     plt.figure(figsize=(10, 6))
     plt.plot(t, theta)
     plt.xlabel('Time (s)')
